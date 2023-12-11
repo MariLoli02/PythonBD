1c3b66c88680c596e1cf8a812e714ae615023ddc
MariLoli02 <84902607+MariLoli02@users.noreply.github.com>
2023-12-11 20:40:04 +0100

    Delete parajson.sh

--- parajson.sh
+++
@@ -1,7 +0,0 @@
-$headers = @{
-    "Content-Type" = "application/json"
-}
-
-$data = ' {"id": 1, "nombre": "Juan Pérez"}'
-
-Invoke-WebRequest -Uri "http://127.0.0.1:5000/api/data" -Method Post -Headers $headers -Body $data
